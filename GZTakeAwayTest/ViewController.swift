839d3df0531f57aab808d1823133f210cf499cd0
absarrahman <raman040635@gmail.com>
2024-02-18 14:31:24 +0600

    Initial Commit

---
+++ GZTakeAwayTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GZTakeAwayTest
+//
+//  Created by Moh. Absar Rahman on 18/2/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
