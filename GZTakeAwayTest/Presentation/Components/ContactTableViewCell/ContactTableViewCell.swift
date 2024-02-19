caf0aa6bfca16fbedd65360b171c278c5e42c2a0
absarrahman <raman040635@gmail.com>
2024-02-19 12:26:27 +0600

    feat: Load image from url (#4)
    Signed-off-by: absarrahman <raman040635@gmail.com>

--- GZTakeAwayTest/Presentation/Components/ContactTableViewCell/ContactTableViewCell.swift
+++ GZTakeAwayTest/Presentation/Components/ContactTableViewCell/ContactTableViewCell.swift
@@ -6,12 +6,13 @@
 //
 
 import UIKit
+import SDWebImage
 
 class ContactTableViewCell: UITableViewCell {
     
     static let identifier = "ContactTableViewCell"
     
-  
+    
     @IBOutlet weak var userImageView: UIImageView!
     
     
@@ -21,21 +22,33 @@ class ContactTableViewCell: UITableViewCell {
     @IBOutlet private weak var phoneNumberLabel: UILabel!
     
     
-
+    
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
     }
-
+    
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
-
+        
         // Configure the view for the selected state
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        // Since width and height are equal. Setting cornerRadius to half of height will make it circular
+        userImageView.layer.cornerRadius = userImageView.bounds.height / 2
     }
     
     func configure(_ model: ContactModel?) {
         nameLabel.text = model?.fullName
         phoneNumberLabel.text = model?.phoneNumber?.formatPhoneNumber
+        setImageFromUrl(urlString: model?.image ?? "")
+    }
+    
+    // Sets image using image url fetched from the model
+    private func setImageFromUrl(urlString: String) {
+        userImageView.sd_setImage(with: URL(string: urlString), placeholderImage: nil, options: [.progressiveLoad])
     }
     
 }
