91aec58b908a1c42c6e09fc50fea552ae151df90
absarrahman <raman040635@gmail.com>
2024-02-19 14:02:47 +0600

    feat: Design reusable button (#10)
    Signed-off-by: absarrahman <raman040635@gmail.com>

---
+++ GZTakeAwayTest/Presentation/Components/GZContactButton/GZContactButton.swift
@@ -0,0 +1,61 @@
+//
+//  GZContactButton.swift
+//  GZTakeAwayTest
+//
+//  Created by Moh. Absar Rahman on 19/2/24.
+//
+
+import UIKit
+
+@IBDesignable
+class GZContactButton: UIButton {
+    
+    @IBOutlet private weak var view: UIView!
+    
+    
+    @IBOutlet weak var buttonLabel: UILabel!
+    
+    
+    @IBOutlet weak var buttonImageView: UIImageView!
+    
+    @IBInspectable var buttonImage: UIImage = UIImage(systemName: "person")!
+    @IBInspectable var buttonText: String = ""
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+    
+    
+    override var intrinsicContentSize: CGSize {
+        CGSize(width: 100, height: 36)
+    }
+    
+    fileprivate func setupNib() {
+        let bundle = Bundle(for: SearchFieldView.self)
+        bundle.loadNibNamed("GZContactButton", owner: self)
+        addSubview(view)
+        setTitle("", for: .normal)
+        view.frame = self.bounds
+    }
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setupNib()
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        setupNib()
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        view.layer.cornerRadius = 8
+        buttonImageView.image = buttonImage
+        buttonLabel.text = buttonText
+    }
+
+}
