3a7a72e68a938d39b808b671df3dc088096c6725
absarrahman <raman040635@gmail.com>
2024-02-19 13:17:07 +0600

    feat: Initial design of contact details (#10)
    Signed-off-by: absarrahman <raman040635@gmail.com>

---
+++ GZTakeAwayTest/Presentation/Components/GZContactButton.swift
@@ -0,0 +1,20 @@
+//
+//  GZContactButton.swift
+//  GZTakeAwayTest
+//
+//  Created by Moh. Absar Rahman on 19/2/24.
+//
+
+import UIKit
+
+class GZContactButton: UIButton {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
